5a2b44aece20de149a42354f0226634dd3f61a1c
moira233 <120929308+moira233@users.noreply.github.com>
2026-02-27 13:28:24 +0000

    Deploying to gh-pages from @ moira233/NASA@0aa090959f9ca1f6752f52c936bd0f265cef589a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "moira233/NASA",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
